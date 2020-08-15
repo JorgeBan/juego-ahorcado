8d15b932f4db04266765ae5f6063b69e3021f26f
JorgeBanegas <banegas392@gmail.com>
2020-08-14 23:04:07 -0400

    primer commit

---
+++ poyecto ahorcado/app/src/main/java/com/juego/elahorcado/MainActivity.java
@@ -0,0 +1,14 @@
+package com.juego.elahorcado;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
